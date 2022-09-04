7f2fb748b01c228a7af44ba45f239e4b0161f139
RinkM <mikerinkert@gmail.com>
2022-09-04 12:46:35 -0600

    changed cardBuilder to make post its

--- script2.js
+++ script2.js
@@ -3,16 +3,16 @@ let cardLibrary = [{
     "projectName": "Calculator",
     "previewLink": "https://htmlpreview.github.io/?https://github.com/RinkM/Odin5-Calculator/blob/main/app.html",
     "projectGithub": "https://github.com/RinkM/Odin5-Calculator",
-    "projectDescription": "A fully working calculator. Created with JS, HTML, and CSS",
-    "projectThumb": "images/fishAvatarSmall.png",
+    "projectDescription": "A fully working calculator. Constructed using mainly JavaScript with some HTML and CSS.",
+    "projectThumb":"images/projectImgs/calcIcon.png",
     },
     {
     "cardNumber": 1,
     "projectName": "Dogs Without Laps",
     "previewLink": "https://htmlpreview.github.io/?https://github.com/RinkM/Odin6-SubscriptionForm/blob/main/index.html",
     "projectGithub": "https://github.com/RinkM/Odin6-SubscriptionForm",
-    "projectDescription": "A subscription form to a fake site. Created with JS, HTML, and CSS",
-    "projectThumb": "images/fishAvatarSmall.png",
+    "projectDescription": "A subscription form to an imaginary charity site. Created with JS, HTML, and CSS",
+    "projectThumb":"images/projectImgs/dogIcon.png",
     },
     {
     "cardNumber": 2,
@@ -20,7 +20,7 @@ let cardLibrary = [{
     "previewLink": "https://htmlpreview.github.io/?https://github.com/RinkM/Odin4-Drawing/blob/main/index.html",
     "projectGithub": "https://github.com/RinkM/Odin4-Drawing",
     "projectDescription": "A drawing app. Created with JS, HTML, and CSS",
-    "projectThumb": "images/fishAvatarSmall.png",
+    "projectThumb":"images/projectImgs/sketchIcon.png",
     },
     {
     "cardNumber": 3,
@@ -28,9 +28,18 @@ let cardLibrary = [{
     "previewLink": "https://htmlpreview.github.io/?https://github.com/RinkM/Odin3-RPS/blob/main/index.html",
     "projectGithub": "https://github.com/RinkM/Odin3-RPS",
     "projectDescription": "Rock, Paper, Scissors. Created with JS, HTML, and CSS",
+    "projectThumb":"images/projectImgs/rpsIcon.png",
+    },
+    {
+    "cardNumber": 4,
+    "projectName": "Robot Dating Website",
+    "previewLink": "https://htmlpreview.github.io/?https://github.com/RinkM/Odin3-RPS/blob/main/index.html",
+    "projectGithub": "https://github.com/RinkM/Odin3-RPS",
+    "projectDescription": "Used React to display and filter users, create forms, and display this information via tabs. ",
     "projectThumb": "images/fishAvatarSmall.png",
     }
 ]
+
 
 const projectForm = document.getElementById('newProjectContainer')
 const mainWindow = document.getElementById('mainWindow')
@@ -89,39 +98,29 @@ let projectName = document.getElementsByClassName("projectName")[0];
 
 
 
+const stickyColors = ["orangeBG", "lightPink", "darkPink", "blue", "dullYellow", "brightYellow"]
 
-
-
-
+function randColor (){
+    const num = Math.floor(Math.random()*6)
+    return stickyColors[num]
+}
 
 // pass the number in the cardLibrary
 function cardBuilder(card){
 
+    // projectCard = card body
     projectCard = document.createElement("div");
     projectCard.setAttribute("id", `card${card.cardNumber}` );
     projectCard.classList.add("projectCard");
+    projectCard.classList.add(randColor());
 
+    // Makes card header with Project Name
     cardHeader = document.createElement("div");
     cardHeader.classList.add("cardHeader");
 
-    projectAvatar = document.createElement("div");
-    projectAvatar.classList.add("projectAvatar");
-    
     projectName = document.createElement("div");
     projectName.classList.add("projectName");
     projectName.textContent = card.projectName
-
-    avatarLink = document.createElement("a");
-    avatarLink.href = `${card.previewLink}`
-    avatarLink.classList.add("imgProjectAvatar");
-
-    imgAvatar = document.createElement('img');
-    imgAvatar.src = card.projectThumb;
-    imgAvatar.setAttribute("alt","Project Avatar");
-    imgAvatar.classList.add("imgProjectAvatar");
-
-    avatarLink.appendChild(imgAvatar);
-    avatarLink.appendChild(projectName);
 
     cardBody = document.createElement("div");
     cardBody.classList.add("cardBody");
@@ -130,25 +129,64 @@ function cardBuilder(card){
     cardText.classList.add("cardText");
     cardText.textContent = card.projectDescription;
 
-    ghLink = document.createElement("a");
-    ghLink.setAttribute("class","githubLogo");
-    ghLink.href = `${card.projectGithub}`
 
+    // Make project Thumb with avatarlink + img    
+    cardLinks = document.createElement("div");
+    cardLinks.classList.add("cardLinks");
+
+    projectThumbnail = document.createElement("div");
+    projectThumbnail.classList.add("projectThumbnail");
+    projectThumbnail.classList.add("grid");
+    projectThumbnail.textContent = "View App"
+    
+    avatarLink = document.createElement("a");
+    avatarLink.href = `${card.previewLink}`
+    avatarLink.classList.add("imgProjectThumb");
+
+    imgAvatar = document.createElement('img');
+    imgAvatar.src = card.projectThumb;
+    imgAvatar.setAttribute("alt","Project Avatar");
+    imgAvatar.classList.add("imgProjectThumb");
+
+    avatarLink.appendChild(imgAvatar);
+    avatarLink.appendChild(projectName);
+
+    // GitHub Logo 
+    ghLinkDiv = document.createElement("div");
+    ghLinkDiv.classList.add("ghLinkDiv");
+    ghLinkDiv.classList.add("centerText");
+    ghLinkDiv.classList.add("grid");
+    ghLinkDiv.textContent = "Inspect Code";
+
+    ghLink = document.createElement("a");
+    ghLogo = document.createElement('img');
+
+    
+    // ghLink.setAttribute("class","githubLink");
+    ghLink.href = `${card.projectGithub}`
+    
     ghLogo = document.createElement('img');
     ghLogo.setAttribute("src", 'images/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png');
     ghLogo.setAttribute("alt","Git Hub Logo");
     ghLogo.classList.add("githubLogo");
 
+    // post Giithub into its kDiv
     ghLink.appendChild(ghLogo);
+    ghLinkDiv.appendChild(ghLink);
 
-// Post into Card header
-    projectAvatar.appendChild(avatarLink);
+// Post name into Card header
+    
     cardHeader.appendChild(projectName);
-    cardHeader.appendChild(projectAvatar);
+    // cardHeader.appendChild(projectThumbnail);
 
 // Post into Card body
+    projectThumbnail.appendChild(avatarLink);
+    cardLinks.appendChild(projectThumbnail);
+    cardLinks.appendChild(ghLinkDiv);
     cardBody.appendChild(cardText);
-    cardBody.appendChild(ghLink);
+    cardBody.appendChild(cardLinks);
+    
+    
 
 // Post header and body onto Card
     projectCard.appendChild(cardHeader);
